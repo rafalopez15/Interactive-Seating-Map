df76f76a5fac419033d8f10370a77938839a72d0
Amin Karabash <aminkarabash@Osiris-MacBook-Pro.local>
2017-02-23 14:09:17 -0700

    Event Class

---
+++ Event.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+
+public class Event extends JFrame implements ActionListener{
+	
+	private boolean isSoldOut = false;
+	
+	
+	public Event(){
+		
+	}
+
+}
