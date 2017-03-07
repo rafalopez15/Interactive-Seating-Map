8c62e92124025598b1a506523933acbac4ff157e
Andrew A. Duron <aaduron2@miners.utep.edu>
2017-03-07 16:20:04 -0700

    Fixed Event.java, MovieEvent.java

--- Event.java
+++ Event.java
@@ -4,122 +4,130 @@ import java.awt.*;
 
 
 public class Event extends JFrame implements ActionListener{
-	
-	private JButton startButton = new JButton("Start");
-	private JButton backButton = new JButton("Back");
-	private JButton[][] grid = new JButton[1][3];
-	private JLabel statusBar = new JLabel("");
-	private boolean isSoldOut = false;
-	public static final int eventOne = 0;
-	public static final int eventTwo = 1;
-	public static final int eventThree = 2;
-	private int maxSeats = 100;
-	private int human = 0;
-
-	private String chars = "X"; 
-
-	
-	public Event(){
-
-		User user = new User("Name", false);
-		setTitle("Events");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setResizable(true);
-
-		JCheckBox vipBox = new JCheckBox("VIP", false);
-		
-		// 
-		JPanel centerPanel = new JPanel(new GridLayout(1, 3));
-		Font font = new Font("MS Gothic", Font.BOLD, 40);
-		for (int i = 0; i < 1; i++){
-			for (int j = 0; j < 3; j++) {
-				grid[i][j] = new JButton(" ");
-				grid[i][j].setFont(font);
-				grid[i][j].addActionListener(this);
-				centerPanel.add(grid[i][j]);
-			}
-		}
-
-		startButton.addActionListener(this);
-
-		JPanel northPanel = new JPanel();
-		northPanel.add(statusBar);
-
-		JPanel southPanel = new JPanel();
-		southPanel.add(startButton);
-		southPanel.add(vipBox);
-
-		// Check to see if the user is a VIP
-		if (vipBox.isSelected())
-			user.setVIP(true);
-
-		setStatus("Click Start to view events");
-		setGridEnabled(false);
-
-		add(northPanel, "North");
-		add(centerPanel, "Center");
-		add(southPanel, "South");
-		
-		
-		
-		setSize(600, 600);
-
-		setLocationRelativeTo(null);
-		
-	}
-
-	private void setStatus(String s) {
-		statusBar.setText(s);
-	}
-
-	public void setGridEnabled(boolean enabled) {
-		for (int i = 0; i < 1; i++)
-			for (int j = 0; j < 3; j++) {
-				grid[i][j].setEnabled(enabled);
-				if (enabled){
-					grid[0][0].setText(" Movie ");
-					grid[0][1].setText(" Game ");
-					grid[0][2].setText(" Concert ");
-				}
-			}
-	}
-
-	private void clickOne(int i, int j) {
-		
-		grid[i][j].setText(chars);	
-		
-	}
-
-
-	private void start() {
-		setStatus("Please select event type");
-		setGridEnabled(true);
-		//isPlay = true;
-	}
-
-
-	public void actionPerformed(ActionEvent event) {
-		// checks to see if play button was pressed
-		if (event.getSource() == startButton) {
-			start();
-			
-		//checks to see if reset button was pressed
-		} 
-			
-		// checks to see which space on the game board was pressed and whose turn it is 
-		else {
-			for (int i = 0; i < 1; i++)
-				for (int j = 0; j < 3; j++)
-					if (event.getSource() == grid[i][j]) {
-						clickOne(i, j);
-					}
-
-		}
-	}
-
-	public static void main (String [] args){
-		new Event().setVisible(true);
-		
-	}
-
+  
+  private JButton startButton = new JButton("Start");
+  private JButton backButton = new JButton("Back");
+  private JButton[][] grid = new JButton[1][3];
+  private JLabel statusBar = new JLabel("");
+  JPanel centerPanel = new JPanel(new GridLayout(1, 3));
+  JPanel newContent = new MovieEvent();
+  private boolean isSoldOut = false;
+  public static final int eventOne = 0;
+  public static final int eventTwo = 1;
+  public static final int eventThree = 2;
+  private int maxSeats = 100;
+  private int human = 0;
+  
+  private String chars = "X"; 
+  
+  
+  public Event(){
+    
+    setTitle("Events");
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setResizable(true);
+    
+    JCheckBox vipBox = new JCheckBox("VIP", false);
+    // 
+    Font font = new Font("MS Gothic", Font.BOLD, 40);
+    for (int i = 0; i < 1; i++){
+      for (int j = 0; j < 3; j++) {
+        grid[i][j] = new JButton(" ");
+        grid[i][j].setFont(font);
+        grid[i][j].addActionListener(this);
+        centerPanel.add(grid[i][j]);
+      }
+    }
+    
+    startButton.addActionListener(this);
+    
+    JPanel northPanel = new JPanel();
+    northPanel.add(statusBar);
+    
+    JPanel southPanel = new JPanel();
+    southPanel.add(startButton);
+    
+    // Check to see if the user is a VIP
+    /*if (vipBox.isSelected())
+      User.setVIP(true);
+    */
+    setStatus("Click Start to view events");
+    setGridEnabled(false);
+    
+    add(northPanel, "North");
+    add(centerPanel, "Center");
+    add(southPanel, "South");
+    
+    
+    
+    setSize(600, 600);
+    
+    setLocationRelativeTo(null);
+    
+  }
+  
+  private void setStatus(String s) {
+    statusBar.setText(s);
+  }
+  
+  public void setGridEnabled(boolean enabled) {
+    for (int i = 0; i < 1; i++)
+      for (int j = 0; j < 3; j++) {
+      grid[i][j].setEnabled(enabled);
+      if (enabled){
+        grid[0][0].setText(" Movie ");
+        grid[0][1].setText(" Game ");
+        grid[0][2].setText(" Concert ");
+      }
+    }
+  }
+  
+  private void clickOne(int i, int j) {
+    
+    //grid[i][j].setText(chars); 
+    if (i == 0 && j == 0) {
+      newContent.setPreferredSize(null);
+      add(newContent, BorderLayout.CENTER);
+      newContent.setVisible(true);
+      centerPanel.setVisible(false);
+      pack();
+    }
+    
+  }
+  
+  
+  private void start() {
+    setStatus("Please select event type");
+    centerPanel.setVisible(true);
+    setGridEnabled(true);
+    //isPlay = true;
+  }
+  
+  
+  public void actionPerformed(ActionEvent event) {
+    // checks to see if play button was pressed
+    if (event.getSource() == startButton) {
+      start();
+      
+      //checks to see if reset button was pressed
+    } 
+    
+    // checks to see which space on the game board was pressed and whose turn it is 
+    else {
+      for (int i = 0; i < 1; i++) {
+        for (int j = 0; j < 3; j++) {
+          if (event.getSource() == grid[i][j]) {
+            clickOne(i, j);
+          }
+        }
+      }
+    }
+    
+    
+  }
+  
+  public static void main (String [] args){
+    new Event().setVisible(true);
+  }
+  
 }
--- MovieEvent.java
+++ MovieEvent.java
@@ -1,20 +1,47 @@
-import javax.swing.JPanel;
-import javax.swing.JButton;
-import java.awt.GridLayout;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
-public class MovieEvent extends Event {
-	
-	Seat[] seats = new Seat[50];
-
-	public MovieEvent() {
-		JPanel panel = new JPanel();
-		panel.setLayout(new GridLayout(10, 5));
-		for (int i = 0; i < buttons.length; i++) {
-			seats[i].setPrice(10.0);
-			panel.add(seats[i]);
-		}
-		panel.setVisible(true);
-		panel.show();
-	}
-
+public class MovieEvent extends JPanel {
+ 
+ Seat[] seats = new Seat[50];
+  JButton back = new JButton("Back");
+  JPanel order = new JPanel();
+  JLabel orderTotal = new JLabel();
+  double total = 0.0;
+  Dimension size = new Dimension(500, 500);
+  
+  public MovieEvent() {
+    back.setPreferredSize(new Dimension(75, 50));
+    back.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == back){
+          setVisible(false);
+        }
+      }
+    });
+    JPanel centerPanel = new JPanel();
+    centerPanel.setPreferredSize(size);
+    centerPanel.setLayout(new BorderLayout());
+    centerPanel.add(getSeats(), BorderLayout.CENTER);
+    centerPanel.add(order, BorderLayout.SOUTH);
+    order.add(back);
+    order.add(new JLabel("Total: "));
+    order.add(new Order(seats));
+    
+    add(centerPanel);
+  }
+  
+  public JComponent getSeats() {
+    JPanel gridSeats = new JPanel();
+    gridSeats.setLayout(new GridLayout(10, 5));
+    for (int i = 0; i < seats.length; i++) {
+      seats[i] = new Seat();
+      seats[i].setPrice(10.0);
+      seats[i].setText("");
+      gridSeats.add(seats[i]);
+    }
+    return gridSeats;
+  }
+  
 }
