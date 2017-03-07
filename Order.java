60613b951531a5777441bdbc1a45d0ed9f0791a5
Andrew A. Duron <aaduron2@miners.utep.edu>
2017-03-07 14:52:04 -0700

    Deleted commented out code

--- Order.java
+++ Order.java
@@ -6,19 +6,11 @@ public class Order extends JLabel {
   
   double total;
   JLabel orderTotal;
-  /*
-  public Order(ChangedSeat s){
-    setText("" + s.getPrice());
-  }
-  */
   
   public Order(ChangedSeat[] s) {
-    //setText("Total: ");
-    //setText("" + s.getPrice());
     for (int i = 0; i < s.length; i++) {
       s[i].addActionListener(new ActionListener() {
         public void actionPerformed(ActionEvent e) {
-        
           ChangedSeat check = (ChangedSeat)e.getSource();
           if (check.isSelected()) {
             check.setText(" ");
