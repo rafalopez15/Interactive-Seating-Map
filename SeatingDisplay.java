dd5497849db034b3bbe9fb99c1bd65ba6c2fb705
Amin Karabash <aminkarabash@Osiris-MacBook-Pro.local>
2017-03-09 14:42:05 -0700

    Deleted code from Seating display and Event Game

--- SeatingDisplay.java
+++ SeatingDisplay.java
@@ -7,53 +7,48 @@ public class SeatingDisplay extends JFrame implements ActionListener  {
 
 	
 	
+	Seat [] northSeating = new Seat[5];
+	Seat [] upperNorthSeating = new Seat[10];
+	Seat [] eastSeating = new Seat[30];
+	
+	
+	
+	public static Dimension upperD = new Dimension(600,150);
+	public static Dimension northSouthD = new Dimension(400,150);
+	public static Dimension eastWestD = new Dimension(400,500);
+	
+	
+	JPanel order = new JPanel();
+	
+	
+	
 	public SeatingDisplay(int x, int y){
 		
-		
+		setTitle("Section");
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLayout(new BorderLayout());
 		
-		JPanel centerPanel = new JPanel(new GridLayout(x, y));
-		JToggleButton[][] grid = new JToggleButton[x][y];
 	
-		int nsx = EventGame.nsx;
-		int y1 = EventGame.y1;
-		int y2 = EventGame.y2;
-		int ewx = EventGame.ewx;
-		int ewy = EventGame.ewy;
 		
-		Dimension upperD = EventGame.upperD;
-		Dimension northSouthD = EventGame.northSouthD;
-		Dimension eastWestD = EventGame.eastWestD;
-		
-		
-		
-		if(x == nsx && y == y2){
+		if(x == EventGame.nsx && y == EventGame.y2){
 			this.setSize(upperD);
 		}
-		if(x == nsx && y == y1){
+		if(x == EventGame.nsx && y == EventGame.y1){
 			this.setSize(northSouthD);
 		}
-		else if(x == ewx && y == ewy){
+		else if(x == EventGame.ewx && y == EventGame.ewy){
 			this.setSize(eastWestD);
 		}
 		
-		Font font = new Font("MS Gothic", Font.BOLD, 10);
-		for (int i = 0; i < x; i++){
-			for (int j = 0; j < y; j++) {
-				grid[i][j] = new JToggleButton(" ");
-				grid[i][j].setFont(font);
-				grid[i][j].addActionListener(this);
-				centerPanel.add(grid[i][j]);
-			}
-		}
-		add(centerPanel, BorderLayout.CENTER);
-		//seatingDisplay.setSize(new Dimension(500,500));
+		
 		setVisible(true);
 		setLayout(null);
 		
 	}
+	
+	
+	
 	
 	
 	public void actionPerformed(ActionEvent e) {
